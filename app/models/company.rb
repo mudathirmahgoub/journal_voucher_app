0bc83139c53a2b5cc731c181d5b6a68f35608447
mudathirmahgoub <mudathirmahgoub@gmail.com>
2017-11-10 10:29:25 -0600

    Added some validation for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ActiveRecord::Base
+  validates :name, :presence => true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: {with: VALID_EMAIL_REGEX}
+  validates :start_date, :presence => true
+  validates :default_currency, :presence => true, length: {maximum: 3, minimum: 3}
 end
